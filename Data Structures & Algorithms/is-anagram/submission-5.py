db1384c390945e91729f497eba45fbd94a392bf7
Hasan <46130250+hasan-mustafaa@users.noreply.github.com>
2026-05-15 10:33:55 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq1 = Counter(s)
+        freq2 = Counter(t)
+
+        freq1 = dict(sorted(freq1.items()))
+        freq2 = dict(sorted(freq2.items()))
+
+        return freq1 == freq2
