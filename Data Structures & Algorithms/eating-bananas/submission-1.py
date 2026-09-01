2201f36b8766c852b971f2ba6af40d15c2d84789
Hasan <46130250+hasan-mustafaa@users.noreply.github.com>
2026-09-01 16:48:13 -0400

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,40 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, sum(piles)
+
+        def feasible(mid):
+            total_hours = 0
+            for pile in piles:
+                total_hours += math.ceil(pile/mid)
+            
+            if total_hours <= h:
+                return True
+            else:
+                return False
+
+
+        while l < r:
+            mid = (r + l) // 2
+
+            if feasible(mid):
+                r = mid
+            else:
+                l = mid + 1
+        
+        return l
+
+
+
+
+
+
+
+
+
+
+    """
+    2. Return k
+    """
+    
