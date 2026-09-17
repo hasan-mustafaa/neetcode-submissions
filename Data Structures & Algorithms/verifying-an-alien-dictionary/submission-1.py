c38e4c946e584b87f2f12e99b43fd35c1fb6332b
Hasan <46130250+hasan-mustafaa@users.noreply.github.com>
2026-09-16 20:41:56 -0400

    Add: verifying-an-alien-dictionary - submission-1

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        orderIndex = {word:index for index, word in enumerate(order)}
+
+        for i in range(len(words) - 1):
+            word1, word2 = words[i], words[i+1]
+
+            for j in range(len(word1)):
+
+                if j == len(word2):
+                    return False
+
+                if word1[j] != word2[j]:
+                    index1, index2 = orderIndex[word1[j]], orderIndex[word2[j]]
+                    if index2 < index1:
+                        return False
+                    break
+        
+        return True
+                
